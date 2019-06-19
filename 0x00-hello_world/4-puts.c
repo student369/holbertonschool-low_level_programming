8327841542ba1b20337f3dbe890b312d09bfbad8
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-19 21:16:39 +0000

    adding a sript that print a text without use the printf function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Author: José Calderón
+  * Description: A C file that print a text
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	puts("Programming is like building a multiliangual puzzle");
+	return (0);
+}
