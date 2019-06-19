59ae264070a623d8af461a783dd70102361aa2e9
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-19 21:28:57 +0000

    adding a script that show a text with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Author: José Calderón
+  * Description: A C file that print a text
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
