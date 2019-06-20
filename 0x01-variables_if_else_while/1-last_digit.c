573d3ff02b03044674158a3f3d44caa2fe61f550
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-20 19:16:20 +0000

    adding a C source code that show if the random number is greater that 5, is equal to 0 or is less to 6

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * author: José Calderón (based in jbarbier code)
+  * Description: A C program to show if the
+  * number n is higher that 5 or zero
+  * n change in each execution randomly
+  * Return: 0
+  */
+int main(void)
+{
+	int n;
+	int l;
+	char *pre = "Last digit of ";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+	if (n > 5)
+	{
+		printf("%s %d is %d and is greater that 5\n", pre, n, l);
+	}
+	else if (n == 0)
+	{
+		printf("%s %d is %d and is 0\n", pre, n, l);
+	}
+	else
+	{
+		printf("%s %d is %d and is less that 6 and not 0\n", pre, n, l);
+	}
+	return (0);
+}
