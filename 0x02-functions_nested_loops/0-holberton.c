e7aa029bab615df5740fd4cf2d94bf8f8e4430a8
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-24 16:24:13 -0500

    adding a first file to show Holberton in C

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: A program that
+ * show Holberton in the stdout
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char *t = "Holberton\n";
+	int l = strlen(t);
+	int i;
+
+	for (i = 0;  i <= l; i++)
+	{
+		_putchar(t[i]);
+	}	
+	return (0);
+}
