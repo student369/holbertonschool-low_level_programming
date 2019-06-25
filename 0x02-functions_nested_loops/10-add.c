7d6c0ae569e958f067a9d1e2799e4d4e3cd9d1b4
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-25 13:21:16 -0500

    adding a sum function to two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * add - funtion to add two numbers
+ * @x: The first number to operate
+ * @y: The second number to operate
+ *
+ * Return: the sum result.
+ */
+int add(int x, int y)
+{	
+	return (x + y);
+}
