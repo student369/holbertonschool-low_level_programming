c40d229bc610926e0faaeabf70a4d6e4db361c6d
Jose Calderon <jose.calderon@holbertonschool.com>
2019-06-27 14:16:44 -0500

    adding a function that multiply the two factors given

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - funtion to multiply two numbers
+ * Description: A function to multiply
+ * two integer numbers.
+ * @a: The first factor
+ * @b: The second factor
+ * Return: the multiply result.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
