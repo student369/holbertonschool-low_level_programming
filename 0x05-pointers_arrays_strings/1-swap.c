1b5e7bbc288019b70e867e9877bc6548da916ce2
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-02 09:45:29 -0500

    adding a function that swap to integer variables given

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - funtion to swap the values of two
+ * integers
+ * Description: A function to swap the values
+ * of two integers.
+ * @a: The first integer
+ * @b: The second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+	*a = *(a + 1);
+	*b = p;
+}
