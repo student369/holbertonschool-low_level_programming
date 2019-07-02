ec71279c991019d4d69dc1d885b1b59bf3951a7b
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-01 23:14:35 -0500

    adding a function that change the value of the integer pointer given to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - funtion to change the value of the
+ * int pointer
+ * Description: A function to change the integer
+ * pointer value given to 98.
+ * @n: The integer pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
