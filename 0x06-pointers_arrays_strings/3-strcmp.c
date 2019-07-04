1e065b809c66b46427a6a62ad47c036496a0c394
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-04 21:42:54 +0000

    adding a function that return the comparation of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - function to concatenate two strings.
+ * @s1: the first string to compare
+ * @s2: the second string to compare
+ * Return: The concatenated string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return ((s1[0] - s2[0]));
+}
