2a44169d9dd54df94607dc19cbbfc3ffc5b64f48
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-04 19:35:59 -0500

    adding a function that encode the text to 1337 number

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * leet - function to encode a text to 1337
+ * @s: the text to encoding
+ * Return: The concatenated string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+
+	char *s1 = "aeotlAEOTL";
+	char *s2 = "4307143071";
+
+	for (s[i] != '\0')
+	{
+		for (s1[j] != '\0')
+		{
+			if (s[i] == s1[j])
+				s[i] = s2[j];
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
