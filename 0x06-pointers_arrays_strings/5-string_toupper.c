13dbdd906c17eacb9282c8d8b54f7e2a83707ee3
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-05 18:53:30 +0000

    updating the function with other adicional function used before

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,9 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	for (s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (_islower(s[i]))
 		{
 			s[i] = s[i] - 32;
 		}
@@ -19,4 +19,15 @@ char *string_toupper(char *s)
 		i++;
 	}
 	return (s);
+}
+
+/**
+ * _islower - function to evaluate if a text
+ * is in lowercase.
+ * @c: the character to evaluate
+ * Return: 1 is is or 0 if not
+ */
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z'));
 }
