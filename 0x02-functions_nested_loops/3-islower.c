c0e284252e1d9720f6875944fa92221d1f45ad08
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-05 18:21:34 +0000

    adding a function to verify is the text is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _islower - function to evaluate if a text
+ * is in lowercase.
+ * @c: the character to evaluate
+ * Return: 1 is is or 0 if not
+ */
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z'));
+}
