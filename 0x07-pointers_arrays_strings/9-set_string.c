35c5e14bf60f30bab8a219329581cb0196ff86a8
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-09 03:18:45 +0000

    adding a function that set a value in a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - function to set a value of a pointer
+ * @s: the pointer to set
+ * @to: The char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
