0bbe5edae4a9bf9fd59e2fe5145237167a816227
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-22 18:48:08 +0000

    update to the function to initialize the dog structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,15 @@
 #include "dog.h"
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * init_dog - Function to allocate in memory
- * @b: The bytes to allocate
+ * @d: The dog structure
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: The dog owner
  *
  * Author: José Calderón
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d, char *name, float age, char *owner)
 {
 	(*d).name = name;
 	(*d).age = age;
