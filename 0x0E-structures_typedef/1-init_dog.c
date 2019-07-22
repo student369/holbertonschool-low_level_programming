8c34b52b8152f91a8c919021690e102ca2294ae3
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-22 19:18:13 +0000

    adding a update to the initialize dog structure code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - Function to allocate in memory
  * @d: The dog structure
@@ -11,6 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (name == NULL || age == 0 || owner == NULL)
+		exit(1);
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
