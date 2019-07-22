af24de1520c01c1616ca06a08c24b03d06829e6c
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-22 17:37:06 +0000

    adding a dog structure to the project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure of a dog
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: The dog owner
+ *
+ * Author: José Calderón
+ * Description: A struct to create a dog
+ * taken the name, age and the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
