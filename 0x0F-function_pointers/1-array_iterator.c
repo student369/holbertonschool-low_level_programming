2bba0341ee103d90d4d4a866b8ee6949c29426d5
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-24 18:59:55 +0000

    Adding a function that excute an action in an array given using pointer to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function to loop an array and
+ * do an action
+ * @array: The array to do the action
+ * @size: The size of the array
+ * @action: The action to do in the array
+ * Author: José Calderón
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
