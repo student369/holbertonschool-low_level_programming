b9eefcb77af91c3bfdb0edfaa2cfdf19bf71ac70
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-24 18:32:42 +0000

    Adding a function that print a name using pointers to functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function to print a name
+ * @name: The name to print
+ * @f: The function to use
+ * Author: José Calderón
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
