db56792e2009909477d63ad3b2d07ebfe2f5a114
Jose Calderon <jose.calderon@holbertonschool.com>
2019-07-26 18:26:43 +0000

    Adding the function 1-print_numbers.c that print the numbers given

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function to print the elements fiven
+ * as a parameters.
+ * @separator: The separator to use
+ * @n: The numbers to print separated by commas
+ *
+ * Author: José Calderón
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int i = 0;
+
+	if (separator == NULL)
+		return;
+
+	if (n == 0)
+		return;
+	va_start(vl, n);
+	for (i = 0; i < n; i++)
+		if (i == (n - 1))
+			printf("%d\n", va_arg(vl, int));
+		else
+			printf("%d %s", va_arg(vl, int), separator);
+	va_end(vl);
+}
