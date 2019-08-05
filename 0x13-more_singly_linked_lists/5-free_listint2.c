3f726facf58eb28cd92de8f656127c40e4c4b4ef
Jose Calderon <jose.calderon@holbertonschool.com>
2019-08-05 10:09:31 -0500

    Adding a function that free completetly the linked list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Function to print a list
+ * @head: The linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = (*head);
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
