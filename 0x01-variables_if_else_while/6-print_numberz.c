23af6100a68dca1ddc89ad2ffa53b3e5d3055fa6
Jose Calderon <jose.calderon@holbertonschool.com>
2019-08-07 11:21:41 -0500

    [FEATURE] Adding a program that print the first numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * author: José Calderón
+  * Description: A C program to show
+  * the numbers from 0 to 9
+  * Return: 0
+  */
+int main(void)
+{
+
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
