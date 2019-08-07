15dde8b3217755793411bdce6d94074471b83258
Jose Calderon <jose.calderon@holbertonschool.com>
2019-08-06 22:24:01 -0500

    Adding an additional functions and a structure to add more nodes.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,19 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct listpt_s - singly linked list
+ * @pt: A pointer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listpt_s
+{
+	void *pt;
+	struct listpt_s *next;
+} listpt_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -31,4 +44,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+listpt_t *add_npt(listpt_t **h, const listint_t *p);
+void free_listpt(listpt_t *h);
 #endif
