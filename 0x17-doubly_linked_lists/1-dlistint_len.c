0ea390625abc02d0a1eced35282be50f1a55882d
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-13 10:14:47 -0500

    Adding the 1-dlistint_len.c function.

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - function to get the len of a double linked list
+ * @h: The double linked list
+ *
+ * Return: the number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *c;
+	size_t i = 0;
+
+	if (h == NULL || h->next == NULL)
+		return (0);
+	c = h;
+	while (c)
+	{
+		c = c->next;
+		i++;
+	}
+	return (i);
+}
