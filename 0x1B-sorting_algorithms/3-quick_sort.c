979e5e1e8011e582a2741f7094ddaa1a3241f128
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-29 19:42:23 -0500

    Adding the 3-quick_sort.c file.

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * qs - function that use the recursion
+ * Description: A function that implements the
+ * internal recursion of the quicksort algoritm.
+ * @array: The subarray
+ * @lo: The low value
+ * @hi: The hi value
+ * @os: The original len
+ *
+ * Return: void
+ */
+void qs(int *array, int lo, int hi, int os)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, os);
+		qs(array, lo, p, os);
+		qs(array, p + 1, hi, os);
+	}
+}
+
+/**
+ * partition - function that use the recursion
+ * Description: A function that implements the
+ * internal recursion of the quicksort algoritm.
+ * @array: The subarray
+ * @lo: The low value
+ * @hi: The hi value
+ * @os: The original len
+ *
+ * Return: the partition index
+ */
+
+int partition(int *array, int lo, int hi, int os)
+{
+	int pivot, i, j, tmp = 0;
+
+	pivot = array[lo + (hi - lo) / 2];
+	i = lo - 1;
+	j = hi + 1;
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+		if (i >= j)
+		{
+			return (j);
+		}
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, os);
+	}
+}
+
+/**
+ * quick_sort - function that use the quicksort sort
+ * Description: A function that implements the
+ * quicksort algoritm to organize the array given.
+ * @array: The array to order
+ * @size: The size
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	qs(array, 0, size - 1, size);
+}
