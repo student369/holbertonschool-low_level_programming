0293d5d189d74de5f385825fe375b45501c3622c
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-15 17:16:00 -0500

    Adding the  5-island_perimeter.py file.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A simple function that count the island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Returns the island perimeter"""
+    count = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
+            if grid[i][j] == 1:
+                count = count + 1
+    return ((2 * count) + 2)
