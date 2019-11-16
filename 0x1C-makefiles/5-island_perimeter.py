ef1ff591c3282820f2d3d0d15d389d272cbd447e
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-15 23:09:19 -0500

    [FIX]

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,11 +6,24 @@ A simple function that count the island_perimeter
 
 def island_perimeter(grid):
     """Returns the island perimeter"""
-    count = 0
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[0])):
+    per = 0
+    r = len(grid)
+    if (r - 1) < 1:
+        return (per)
+    c = len(grid[0])
+    if (c - 1) < 1:
+        return (per)
+    if r > 100 or c > 100:
+        return
+    for i in range(r):
+        for j in range(c):
             if grid[i][j] == 1:
-                count = count + 1
-    if count == 0:
-        return (0)
-    return ((2 * count) + 2)
+                if (i - 1) == -1 or grid[i - 1][j] != 1:
+                    per = per + 1
+                if (j + 1) == c or grid[i][j + 1] != 1:
+                    per = per + 1
+                if (i + 1) == r or grid[i + 1][j] != 1:
+                    per = per + 1
+                if j < 0 or grid[i][j - 1] != 1:
+                    per = per + 1
+    return (per)
