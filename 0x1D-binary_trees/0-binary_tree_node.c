db6b8a8170f9fd96b3c45cdffbfb84cda1472e8a
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-02 18:30:48 -0500

    [UPDATE]

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *nw;
 
 	nw = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (nw == NULL)
+		return (NULL);
 	nw->n = value;
 	nw->left = NULL;
 	nw->right = NULL;
