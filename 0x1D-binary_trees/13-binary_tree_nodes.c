20a999f18bfe01b70109447e94992120476be434
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-05 09:10:05 -0500

    [FIX]

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,21 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - function to get the nodes
- * with 1 child in the BT
+ * binary_tree_nodes - function to count the nodes with
+ * one or mode child
  *
- * @tree: The tree.
+ * @tree: The tree
  *
- * Return: The count of leaves
+ * Return: 0 or more
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	int c = 0;
+
 	if (tree == NULL)
 		return (0);
-	if ((tree->left || tree->right) && tree->parent)
-		return (1);
-	if (!tree->left || !tree->right)
-		return (0);
-	return (binary_tree_nodes(tree->left) +
-		binary_tree_nodes(tree->right) + 1);
+	if (tree->left != NULL || tree->right != NULL)
+		c = 1;
+	return (binary_tree_nodes(tree->left)
+		+ c +
+		binary_tree_nodes(tree->right));
 }
