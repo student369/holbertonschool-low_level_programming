7cda3bc79a774ca973bbdc6f8baebe1a21b2e78c
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-05 08:43:49 -0500

    Adding the 13-binary_tree_nodes.c file.

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,43 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - function to get the heiht of a node
+ * binary_tree_nodes - function to get the nodes
+ * with 1 child in the BT
  *
- * @tree: The tree to check
+ * @tree: The tree.
  *
- * Return: 0, 1 or 2
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	int h = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL)
-		h++;
-	if (tree->right != NULL)
-		h++;
-	return (h);
-}
-
-/**
- * binary_tree_nodes - function to count nodes with 1 child
- *
- * @tree: The tree
- *
- * Return: the new node.
+ * Return: The count of leaves
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int c = 0;
-
 	if (tree == NULL)
 		return (0);
-
-	if (binary_tree_height(tree) > 0)
-		c = 1;
-	else
-		c = 0;
-	return (binary_tree_nodes(tree->left) + c +
-		binary_tree_nodes(tree->right));
+	if ((tree->left || tree->right) && tree->parent)
+		return (1);
+	if (!tree->left)
+		return (0);
+	return (binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right) + 1);
 }
