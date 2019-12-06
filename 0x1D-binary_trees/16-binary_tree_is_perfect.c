f6237ccc597cba09c421df56ed2a6b7d9ccb9835
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-06 17:16:38 -0500

    Adding the 16-binary_tree_is_perfect.c

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,85 +1,96 @@
 #include "binary_trees.h"
-
-int binary_tree_is_leaf_depth(const binary_tree_t *node);
+size_t bt_height(const binary_tree_t *tree);
+size_t binary_tree_one_child(const binary_tree_t *tree);
 /**
- * binary_tree_depth - function to get the depth of a node
- *
- * @tree: The tree to check
- *
- * Return: 1 or 0
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	int d = 0;
-
-	if (tree == NULL)
-		return (0);
-	while (tree->parent != NULL)
-	{
-		tree = tree->parent;
-		d++;
-	}
-	return (d);
-}
-
-/**
- * binary_tree_is_leaf_depth - function to check
- * the leads in the  BT and the same depth
- *
- * @node: The node to check
- *
- * Return: the new node.
- */
-int binary_tree_is_leaf_depth(const binary_tree_t *node)
-{
-	int l = 0, cd = 0;
-	static int d = 0, pd;
-
-	if (node == NULL)
-		return (0);
-	if (node->left == NULL)
-		l++;
-	if (node->right == NULL)
-		l++;
-	if (l == 2)
-	{
-		if (d == 0)
-			pd = binary_tree_depth(node);
-		if (d > 0)
-		{
-			cd = binary_tree_depth(node);
-			if (cd != pd)
-			{
-				d++;
-				return (0);
-			}
-		}
-		d++;
-		return (1);
-	}
-	else
-		return (0);
-}
-
-/**
- * binary_tree_leaves - function to count the BT leaves
+ * binary_tree_one_child - function to count the nodes with
+ * one child
  *
  * @tree: The tree
  *
- * Return: the count of leaves or 0
+ * Return: 0 or more
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_one_child(const binary_tree_t *tree)
 {
+	int c = 0;
+
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_leaves(tree->left)
-		+ binary_tree_is_leaf_depth(tree) +
-		binary_tree_leaves(tree->right));
+	if ((tree->left == NULL && tree->right != NULL) ||
+	    (tree->left != NULL && tree->right == NULL))
+		c = 1;
+	return (binary_tree_one_child(tree->left)
+		+ c +
+		binary_tree_one_child(tree->right));
 }
 
 /**
- * binary_tree_is_perfect - function to check if the
- * BT is perfect.
+ * binary_tree_is_full - function to check if the
+ * BT is full.
+ *
+ * @tree: The tree.
+ *
+ * Return: 1 or 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_one_child(tree))
+		return (0);
+	else
+		return (1);
+}
+
+/**
+ * bt_height - function to get the height
+ * of the BT
+ *
+ * @tree: The tree.
+ *
+ * Return: The height
+ */
+size_t bt_height(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL &&
+	    tree->right == NULL)
+		return (0);
+	l = bt_height(tree->left);
+	r = bt_height(tree->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+}
+/**
+ * binary_tree_balance - function to get the balance
+ * factor of the BT
+ *
+ * @tree: The tree.
+ *
+ * Return: The count of leaves
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l, r, b = 0;
+
+	if (tree == NULL)
+		return (0);
+	l = bt_height(tree->left);
+	r = bt_height(tree->right);
+	if (!tree->left)
+		l--;
+	if (!tree->right)
+		r--;
+	b = l - r;
+	return (b);
+}
+/**
+ * binary_tree_is_perfect - function to check it the BT
+ * is perfect.
  *
  * @tree: The tree.
  *
@@ -87,10 +98,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	if (binary_tree_leaves(tree) % 2 == 0)
+	if (binary_tree_is_full(tree) == 1 &&
+	    binary_tree_balance(tree) == 0)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
